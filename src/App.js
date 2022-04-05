6daf49f13a1e43aa95d30fe3d4bb2010401ec923
Katrous <richardslaura24@gmail.com>
2022-04-05 13:03:04 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tours-project'
+function App() {
+
+  
+  const [tours, setTours] = useState([])
+  // fetch api request.
+  // assigned the request response to state
+  // Render state data
+  return <h2>Tours Project Setup</h2>
+}
+
+export default App
